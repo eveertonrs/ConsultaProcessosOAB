e2965551d141755e1305020d0e3eb014d33ccf3a
@eveertonrs <evertonbarbosars@gmail.com>
2023-09-18 21:12:12 -0300

    Adicionado nova tela de login e arrumado as rotas para as paginas, criado usuarios para teste

--- app.py
+++ app.py
@@ -1,35 +1,29 @@
-from flask import Flask, render_template, request, redirect
+from flask import Flask, render_template, request, redirect, flash
+import json
 
 app = Flask(__name__)
+app.config['SECRET_KEY'] = "palavra-secreta123"
 
-# Dicionário para armazenar os usuários (apenas para fins de teste)
-users = {'teste': 'teste'}
-
-@app.route('/')
-def root():
-    return redirect('/login')
-
-@app.route('/login', methods=['GET', 'POST'])
-def login():
-    if request.method == 'POST':
-        username = request.form['username']
-        password = request.form['password']
-
-        # Verifique se o usuário e a senha correspondem aos registros de teste
-        if username in users and users[username] == password:
-            # Se a autenticação for bem-sucedida, redirecione o usuário para a página inicial
-            return redirect('/home')
-        else:
-            # Se a autenticação falhar, exiba uma mensagem de erro
-            error_message = "Usuário ou senha incorretos. Tente novamente."
-            return render_template('login.html', error=error_message)
-    
-    # Se for um pedido GET, apenas renderize a página de login
-    return render_template('login.html')
-
-@app.route('/home')
+@app.route("/")
 def home():
-    return render_template('index.html')
+    return render_template('/login.html')
+
+@app.route("/login", methods=['POST'])
+def login():
+    usuario = request.form.get("username")
+    senha = request.form.get("password")
+    with open('data/usuarios.json') as usuarios:
+        lista = json.load(usuarios)
+        for c in lista:
+            if usuario == c['username'] and senha == c['password']:
+                return render_template("/index.html", nomeUsuario=c['username'])
+        flash('Usuário Inválido')
+    return redirect("/")
+
+@app.route("/logout")
+def logout():
+    return redirect("/")
+
 
 if __name__ == '__main__':
     app.run(debug=True)
