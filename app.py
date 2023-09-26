56988f5104ef75403bcc9bd9e96c6ebf1630e9fa
@eveertonrs <evertonbarbosars@gmail.com>
2023-09-26 18:46:36 -0300

    Feito o banco de dados para a criação de testes de usuario com SQL server, passado o scrapping para o app para testes de trazer os dados

--- app.py
+++ app.py
@@ -1,29 +1,194 @@
 from flask import Flask, render_template, request, redirect, flash
-import json
+from flask_sqlalchemy import SQLAlchemy
+from flask import Flask, jsonify
+from selenium import webdriver
+import time
+import socket
+from selenium.webdriver.support.ui import Select
+from selenium.webdriver.common.by import By
+from flask import Flask, render_template, request, redirect, flash, session
+from threading import Thread
+from selenium.webdriver.chrome.options import Options
+from selenium.webdriver.chrome.service import Service
+from webdriver_manager.chrome import ChromeDriverManager 
+import threading
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = "palavra-secreta123"
+app.config['SQLALCHEMY_DATABASE_URI'] = 'mssql+pyodbc:///?odbc_connect=DRIVER={ODBC Driver 17 for SQL Server};SERVER=localhost;DATABASE=DB_ConsultaOAB;UID=master;PWD=##'
+db = SQLAlchemy(app)
+class Usuario(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(80), unique=True, nullable=False)
+    password = db.Column(db.String(80), nullable=False) 
+
+scraping_lock = threading.Lock()
+
+@app.route("/scraped_data")
+def get_scraped_data():
+    global scraping_lock
+    
+    # Verifique se o scraping está em andamento em outra thread
+    if scraping_lock.locked():
+        flash('O scraping já está em andamento. Aguarde a conclusão.')
+        return redirect("/")
+    
+    # Inicie o scraping em uma thread separada
+    scraping_thread = threading.Thread(target=scrape_data)
+    scraping_thread.start()
+    
+    return redirect("/")
+
+def scrape_data():
+   # Configurar o serviço do Chrome para rodar em segundo plano
+    chrome_service = Service(ChromeDriverManager().install())
+
+    # Configurar as opções do Chrome para o modo headless
+    chrome_options = webdriver.ChromeOptions()
+    chrome_options.add_argument('--headless')
+    chrome_options.add_argument('--disable-gpu')  # Desativar a aceleração de GPU, às vezes é necessário
+
+    # Inicialize o driver do Chrome com as opções configuradas
+    driver = webdriver.Chrome(service=chrome_service, options=chrome_options)
+
+
+        
+    driver.get('https://esaj.tjsp.jus.br/cpopg/open.do')
+    time.sleep(5)
+
+    elemento_select = driver.find_element(By.ID, 'cbPesquisa')
+    select = Select(elemento_select)
+
+    # Selecione a opção com o value igual a "NUMOAB"
+    select.select_by_value('NUMOAB')
+    campo = driver.find_element(By.ID, 'campo_NUMOAB')
+    campo.send_keys('485605')
+
+    btnConsultar = driver.find_element(By.ID, 'botaoConsultarProcessos')
+
+    btnConsultar.click()
+
+    time.sleep(5)
+
+    # Localize a div principal que contém todos os elementos de processo
+    div_processos = driver.find_element(By.ID, 'listagemDeProcessos')
+
+    # Localize todos os elementos de processo (li) dentro da div principal
+    elementos_processo = div_processos.find_elements(By.TAG_NAME, 'li')
+    # Crie uma lista vazia para armazenar os links dos processos
+    links_processos = []
+
+    # Itere sobre os elementos de processo e extraia os links
+    for elemento in elementos_processo:
+        link_processo = elemento.find_element(By.TAG_NAME, 'a').get_attribute('href')
+        links_processos.append(link_processo)
+        print(links_processos)
+
+    # Agora você tem uma lista de links dos processos
+    # Itere sobre esses links e acesse cada página de detalhes do processo
+    for link_processo in links_processos:
+        driver.get(link_processo)
+            
+        # Encontre e extraia as informações da página de detalhes do processo
+        numero_processo = driver.find_element(By.ID, 'numeroProcesso').text
+        classe_processo = driver.find_element(By.ID, 'classeProcesso').text
+        assunto_processo = driver.find_element(By.ID, 'assuntoProcesso').text
+        foro_processo = driver.find_element(By.ID, 'foroProcesso').text
+        vara_processo = driver.find_element(By.ID, 'varaProcesso').text
+        juiz_processo = driver.find_element(By.ID, 'juizProcesso').text
+
+        # Imprima as informações
+        print(f'Número do Processo: {numero_processo}')
+        print(f'Classe do Processo: {classe_processo}')
+        print(f'Assunto do Processo: {assunto_processo}')
+        print(f'Foro do Processo: {foro_processo}')
+        print(f'Vara do Processo: {vara_processo}')
+        print(f'Juiz do Processo: {juiz_processo}')
+        
+        # Encontre e extraia as informações da movimentação na mesma página
+        elementos_movimentacao = driver.find_elements(By.CLASS_NAME, 'containerMovimentacao')
+
+        # Defina o número de primeiras movimentações que você deseja coletar
+        numero_de_movimentacoes = 5  # Altere para o número desejado
+
+        # Itere sobre as primeiras movimentações
+        for elemento in elementos_movimentacao[:numero_de_movimentacoes]:
+            # Encontre os elementos dentro de <tr> que contêm as informações
+            data_movimentacao = elemento.find_element(By.CLASS_NAME, 'dataMovimentacao').text
+            descricao_movimentacao = elemento.find_element(By.CLASS_NAME, 'descricaoMovimentacao').text
+
+            # Imprima as informações da movimentação
+            print(f'Data de Movimentação: {data_movimentacao}\n')
+            print(f'Descrição da Movimentação: {descricao_movimentacao}\n')
+
+        driver.back()
+        
+    # Store the scraped information in a data structure
+    scraped_data = {
+        "numero_processo": numero_processo,
+        "classe_processo": classe_processo,
+        "assunto_processo": assunto_processo,
+        "foro_processo": foro_processo,
+        "vara_processo": vara_processo,
+        "juiz_processo": juiz_processo,
+        "movimentacoes": []
+    }
+    
+    # Iterate over the first few movements and store them in the scraped_data dictionary
+    for elemento in elementos_movimentacao[:numero_de_movimentacoes]:
+        data_movimentacao = elemento.find_element(By.CLASS_NAME, 'dataMovimentacao').text
+        descricao_movimentacao = elemento.find_element(By.CLASS_NAME, 'descricaoMovimentacao').text
+        scraped_data["movimentacoes"].append({
+            "data_movimentacao": data_movimentacao,
+            "descricao_movimentacao": descricao_movimentacao
+        })
+        
+    scraped_data = {
+        # Coloque os dados coletados aqui...
+    }
+    
+    session['scraped_data'] = scraped_data
+
+    # Libere a variável de bloqueio
+    scraping_lock.release()
+
+    # Encerre o driver do Chrome
+    driver.quit()
+
 
 @app.route("/")
 def home():
-    return render_template('/login.html')
+    if 'scraped_data' in session:
+        scraped_data = session['scraped_data']
+        return render_template('/index.html', nomeUsuario=session['username'], scraped_data=scraped_data)
+    else:
+        return render_template('/login.html')
 
 @app.route("/login", methods=['POST'])
 def login():
     usuario = request.form.get("username")
     senha = request.form.get("password")
-    with open('data/usuarios.json') as usuarios:
-        lista = json.load(usuarios)
-        for c in lista:
-            if usuario == c['username'] and senha == c['password']:
-                return render_template("/index.html", nomeUsuario=c['username'])
-        flash('Usuário Inválido')
-    return redirect("/")
+    
+    user = Usuario.query.filter_by(username=usuario).first()
+    
+    if user and user.password == senha:
+        # Store the username in the session variable
+        session['username'] = user.username
+        
+        # Start the scraping process in the background using a separate thread
+        scraping_thread = Thread(target=get_scraped_data)
+        scraping_thread.start()
+        
+        return redirect("/")
+    else:
+        flash('Usuário ou senha inválidos')
+        return redirect("/")
 
 @app.route("/logout")
 def logout():
+    # Clear the session variables
+    session.clear()
     return redirect("/")
-
 
 if __name__ == '__main__':
     app.run(debug=True)
--- scripts/selenium_script.py
+++ scripts/selenium_script.py
@@ -2,13 +2,27 @@ from selenium import webdriver
 import time
 import socket
 from selenium.webdriver.support.ui import Select
-from selenium.webdriver.common.by import By  # Importe a classe By
+from selenium.webdriver.common.by import By
+from selenium import webdriver
+from webdriver_manager.chrome import ChromeDriverManager
+from selenium.webdriver.chrome.service import Service
 
-# Configure o caminho absoluto para o executável do ChromeDriver
-caminho_para_o_executável_do_chromedriver = r'C:\Projeto trabalhos\ConsultarProcessosOAB\chromedriver.exe'
+# Configurar o serviço do Chrome para rodar em segundo plano
+chrome_service = Service(ChromeDriverManager().install())
 
-# Inicialize o driver do Chrome
-driver = webdriver.Chrome()
+# Configurar as opções do Chrome para o modo headless
+chrome_options = webdriver.ChromeOptions()
+chrome_options.add_argument('--headless')
+chrome_options.add_argument('--disable-gpu')  # Desativar a aceleração de GPU, às vezes é necessário
+
+# Inicialize o driver do Chrome com as opções configuradas
+driver = webdriver.Chrome(service=chrome_service, options=chrome_options)
+
+
+# # Configure o caminho absoluto para o executável do ChromeDriver
+# caminho_para_o_executável_do_chromedriver = r'C:\Projeto trabalhos\ConsultarProcessosOAB\chromedriver.exe'
+# # Inicialize o driver do Chrome
+# driver = webdriver.Chrome()
 
 # Agora você pode usar o objeto 'driver' para automatizar o Google Chrome
 # Por exemplo, navegar para uma página da web:
@@ -23,10 +37,12 @@ select = Select(elemento_select)
 # Selecione a opção com o value igual a "NUMOAB"
 select.select_by_value('NUMOAB')
 
+
 campo = driver.find_element(By.ID, 'campo_NUMOAB')
 campo.send_keys('485605')
 
 btnConsultar = driver.find_element(By.ID, 'botaoConsultarProcessos')
+
 btnConsultar.click()
 
 time.sleep(5)
@@ -34,66 +50,74 @@ time.sleep(5)
 # Localize a div principal que contém todos os elementos de processo
 div_processos = driver.find_element(By.ID, 'listagemDeProcessos')
 
-# Obtém todos os links para os detalhes dos processos
-links_processos = driver.find_elements(By.XPATH, '//a[contains(@href, "/cpopg/show.do?processo.numero=")]')
-links_processos = [link.get_attribute('href') for link in links_processos]
+# Localize todos os elementos de processo (li) dentro da div principal
+elementos_processo = div_processos.find_elements(By.TAG_NAME, 'li')
 
-# Agora você tem uma lista de links para os detalhes de cada processo
-# Itere sobre esses links e colete as informações detalhadas
-informacoes_processos = []
+# Variável para armazenar as informações
+# informacoes_processos = []
 
+# print("TODOS OS PROCESSOS")
+# # Itere sobre os elementos de processo e colete as informações
+# for elemento in elementos_processo:
+#     numero_processo = elemento.find_element(By.CLASS_NAME, 'nuProcesso').text.strip()
+#     advogado = elemento.find_element(By.CLASS_NAME, 'nomeParte').text.strip()
+#     classe_processo = elemento.find_element(By.CLASS_NAME, 'classeProcesso').text.strip()
+#     assunto_principal = elemento.find_element(By.CLASS_NAME, 'assuntoPrincipalProcesso').text.strip()
+#     recebido_em = elemento.find_element(By.CLASS_NAME, 'dataLocalDistribuicaoProcesso').text.strip()
+
+#     informacoes_processos.append(f"{numero_processo}\nAdvogado(a):\n{advogado}\n{classe_processo}\n{assunto_principal}\nRecebido em:\n{recebido_em}\n")
+
+# # Imprima as informações coletadas no formato desejado
+# for info in informacoes_processos:
+#     print(info)
+#     print()
+
+# Crie uma lista vazia para armazenar os links dos processos
+links_processos = []
+
+# Itere sobre os elementos de processo e extraia os links
+for elemento in elementos_processo:
+    link_processo = elemento.find_element(By.TAG_NAME, 'a').get_attribute('href')
+    links_processos.append(link_processo)
+    print(links_processos)
+
+# Agora você tem uma lista de links dos processos
+# Itere sobre esses links e acesse cada página de detalhes do processo
 for link_processo in links_processos:
     driver.get(link_processo)
-    informacoes = {}
+        
+    # Encontre e extraia as informações da página de detalhes do processo
+    numero_processo = driver.find_element(By.ID, 'numeroProcesso').text
+    classe_processo = driver.find_element(By.ID, 'classeProcesso').text
+    assunto_processo = driver.find_element(By.ID, 'assuntoProcesso').text
+    foro_processo = driver.find_element(By.ID, 'foroProcesso').text
+    vara_processo = driver.find_element(By.ID, 'varaProcesso').text
+    juiz_processo = driver.find_element(By.ID, 'juizProcesso').text
+
+    # Imprima as informações
+    print(f'Número do Processo: {numero_processo}')
+    print(f'Classe do Processo: {classe_processo}')
+    print(f'Assunto do Processo: {assunto_processo}')
+    print(f'Foro do Processo: {foro_processo}')
+    print(f'Vara do Processo: {vara_processo}')
+    print(f'Juiz do Processo: {juiz_processo}')
     
-    try:
-        # Coleta as informações disponíveis na página de detalhes do processo
-        numero_processo = driver.find_element(By.ID, 'numeroProcesso').text
-        classe_processo = driver.find_element(By.ID, 'classeProcesso').text
-        assunto_processo = driver.find_element(By.ID, 'assuntoProcesso').text
-        foro_processo = driver.find_element(By.ID, 'foroProcesso').text
-        vara_processo = driver.find_element(By.ID, 'varaProcesso').text
-        juiz_processo = driver.find_element(By.ID, 'juizProcesso').text
+    # Encontre e extraia as informações da movimentação na mesma página
+    elementos_movimentacao = driver.find_elements(By.CLASS_NAME, 'containerMovimentacao')
 
-        informacoes['Número do Processo'] = numero_processo
-        informacoes['Classe do Processo'] = classe_processo
-        informacoes['Assunto do Processo'] = assunto_processo
-        informacoes['Foro do Processo'] = foro_processo
-        informacoes['Vara do Processo'] = vara_processo
-        informacoes['Juiz do Processo'] = juiz_processo
-    except:
-        pass
-    
-    try:
-        # Coleta informações de movimentações, se disponíveis
-        movimentacoes = driver.find_elements(By.XPATH, '//div[@class="containerMovimentacao"]')
-        informacoes['Movimentações'] = []
+    # Defina o número de primeiras movimentações que você deseja coletar
+    numero_de_movimentacoes = 5  # Altere para o número desejado
 
-        for movimentacao in movimentacoes[:5]:  # Coleta apenas as 5 primeiras movimentações
-            data_movimentacao = movimentacao.find_element(By.CLASS_NAME, 'dataMovimentacao').text
-            descricao_movimentacao = movimentacao.find_element(By.CLASS_NAME, 'descricaoMovimentacao').text
+    # Itere sobre as primeiras movimentações
+    for elemento in elementos_movimentacao[:numero_de_movimentacoes]:
+        # Encontre os elementos dentro de <tr> que contêm as informações
+        data_movimentacao = elemento.find_element(By.CLASS_NAME, 'dataMovimentacao').text
+        descricao_movimentacao = elemento.find_element(By.CLASS_NAME, 'descricaoMovimentacao').text
 
-            informacoes['Movimentações'].append({
-                'Data de Movimentação': data_movimentacao,
-                'Descrição da Movimentação': descricao_movimentacao
-            })
-    except:
-        pass
+        # Imprima as informações da movimentação
+        print(f'Data de Movimentação: {data_movimentacao}\n')
+        print(f'Descrição da Movimentação: {descricao_movimentacao}\n')
 
-    informacoes_processos.append(informacoes)
+    driver.back()
 
-# Imprime as informações coletadas no formato desejado
-for processo in informacoes_processos:
-    print("Informações do Processo:")
-    for chave, valor in processo.items():
-        if chave == 'Movimentações':
-            print("Movimentações:")
-            for movimentacao in valor:
-                print(f"Data de Movimentação: {movimentacao['Data de Movimentação']}")
-                print(f"Descrição da Movimentação: {movimentacao['Descrição da Movimentação']}")
-        else:
-            print(f"{chave}: {valor}")
-    print()
-
-# Fecha o navegador
 driver.quit()
